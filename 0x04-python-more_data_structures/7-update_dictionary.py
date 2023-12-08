16ecd01b51c0ca5ed6b01b4f3410ed0fb049f3b9
Basma-Shaban <shaabanbasma@gmail.com>
2023-12-08 15:49:51 -0800

    Basma

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+
+"""Replace or add key/value pairs in a dictionary."""
+
+
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
-    return a_dictionary 
+    return (a_dictionary) 
